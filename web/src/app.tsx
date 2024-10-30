d85861b64dc638b6ff612c6c7d7d616ea9578e3b
Vinicius <viniciusmarques.dev@gmail.com>
2024-10-30 13:19:53 -0300

    Added application layout
    The application has been structured into two main folders: "web" and "server." The "web" folder contains all user interface components, including the layout and visual elements of the application. Meanwhile, the "server" folder organizes routes and business logic functionalities, handling data processing and database communication. This clear separation between front-end and back-end facilitates easier maintenance and scalability.

---
+++ web/src/app.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+import { useQueries, useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
+export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 6, //60 segundos
+  })
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      {/*  */}
+
+      {/*  */}
+      <CreateGoal />
+    </Dialog>
+  )
+}
---
+++ web/src/components/summary.tsx
@@ -0,0 +1,104 @@
+import { CheckCircle2, Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import { MyHabitsIcon } from './my-habits-icon'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
+import { OutlineButton } from './ui/outline-button'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from '../http/get-summary'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-BR'
+import { PendingGoals } from './pending-goals'
+
+dayjs.locale(ptBR)
+
+export function Summary() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 6, //60 segundos
+  })
+
+  if (!data) {
+    return null
+  }
+
+  console.log(Object.keys(data.goalsPerDay))
+
+  const firstDayOfWeek = dayjs().startOf('week').format('D MMM')
+  const lastDayOfWeek = dayjs().endOf('week').format('D MMM')
+
+  const completedPercentage = Math.round((data.completed * 100) / data?.total)
+
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <MyHabitsIcon />
+          <span className="text-lg font-semibold capitalize">
+            {firstDayOfWeek} - {lastDayOfWeek}
+          </span>
+        </div>
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={30}>
+          <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou{' '}
+            <span className="text-zinc-100">{data?.completed}</span> de{' '}
+            <span className="text-zinc-100">{data?.total}</span> metas nessa
+            semana.
+          </span>
+          <span>{completedPercentage}%</span>
+        </div>
+
+        <Separator />
+        <PendingGoals />
+
+        <div className="flex flex-col gap-6 ">
+          <h2 className="text-xl font-medium">Sua semana</h2>
+          {Object.entries(data.goalsPerDay).map(([date, goals]) => {
+            const weekDay = dayjs(date).format('dddd')
+            const formattedDate = dayjs(date).format('D[ de ]MMMM')
+
+            return (
+              <div key={date} className="flex flex-col gap-4">
+                <h3 className="font-medium gap-2">
+                  <span className="capitalize">{weekDay} </span>
+                  <span className="text-zinc-400 text-xs">{formattedDate}</span>
+                </h3>
+
+                <ul className="flex flex-col gap-3">
+                  {goals.map(goal => {
+                    const time = dayjs(goal.completedAt).format('HH:mm')
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou{' '}
+                          <span className="text-zinc-100">{goal.title}</span> às{' '}
+                          <span className="text-zinc-100">{time}h</span>
+                        </span>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
